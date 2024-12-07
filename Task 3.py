fbd3ce1abdb573c30a15bed249f29e03f794fd04
unknown <v.duganov2014@yandex.ru>
2024-12-07 23:54:22 +0300

    Changes in Task 3

--- Task 3.py
+++ Task 3.py
@@ -1,71 +1,37 @@
-def opening_and_forming(file_path, dishes, person_count):
-    with open(file_path, encoding='utf-8') as f:
-        data = f.read()
+import os
+from pathlib import Path
 
-    cook_book = {}
-    ingredients = []
-    current_dish = None
-    words = data.split('\n')
-    non_empty_lines = [line for line in words if line.strip()]
+# Чтение всех txt c определенной директории и сортировка значений списка
+def read_files_from_directory(directory):
+    content_list = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+                content_list.append(content)
+    sorted_contents = sorted(content_list, key=lambda x: len(x.split('\n')))
+    return sorted_contents
 
-    for i in non_empty_lines:
-        if '|' in i:
-            parts = [part.strip() for part in i.split('|')]
-            ingridient = {
-                'ingredient_name': parts[0],
-                'quantity': int(parts[1]),
-                'measure': parts[2]
-            }
-            ingredients.append(ingridient)
-        if not i.isdigit() and "|" not in i:
-            if current_dish:
-                cook_book[current_dish] = ingredients
-                ingredients = []
-            current_dish = i
+# Запись output файла
+def write_content_to_file(sorted_contents, output_file_path):
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        for content in sorted_contents:
+            output_file.write(content)
+            output_file.write('\n')
 
-    if current_dish:
-        cook_book[current_dish] = ingredients
-
-    list_by_dishes = {}
-
-    for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                ingredient_name = ingredient['ingredient_name']
-                measure = ingredient['measure']
-                quantity = ingredient['quantity'] * person_count
-
-                if ingredient_name in list_by_dishes:
-                    list_by_dishes[ingredient_name]['quantity'] += quantity
-                else:
-                    list_by_dishes[ingredient_name] = {'measure': measure, 'quantity': quantity}
-        else:
-            print(f"Блюдо '{dish}' не найдено в cook_book.")
-
-    return list_by_dishes
-
-
+# Основная функция для определения параметров
 def main():
-    file_path = 'recipes.txt'
-    dishes = ['Омлет', 'Запеченный картофель']
-    person_count = 2
-    result = opening_and_forming(file_path, dishes, person_count)
-    print(result)
+    directory = 'C:\\Users\\Владислав\\Desktop\\Reading_Writing\\Files'
 
+    sorted_contents = read_files_from_directory(directory)
 
+    output_file_path = os.path.join(directory, 'output.txt')
+
+    write_content_to_file(sorted_contents, output_file_path)
+
+    print(f"Информация из директории {directory} записана в {output_file_path}")
+
+# Проверка, запущен ли скрипт напрямую
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
